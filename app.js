62615cf0cf9f6c3546292765922a8a756c360e59
Erlend <erlendk.kodehode@gmail.com>
2024-10-22 12:30:55 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,286 @@
+gameArray = [
+  {
+    game: "Xenoblade 1",
+    characters: [
+      {
+        character: "Shulk",
+        quotes: [
+          "Long ago, the world was nothing more than an endless sea cloaked in a boundless sky, reaching as far as could possibly be imagined. ... Then two great titans came into existance: the Bionis and the Mechonis. ... The titans were locked in a timeless battle. ... Until at last...only their lifeless corpses remained.",
+          `In my head, there are two versions of me. One of them is saying that. It's telling me, "Listen to what Dunban said". The other one, it keeps shouting, "Make them pay! Destroy every single one of them!". And it won't stop getting louder.`,
+          "I have to know why! What's the reason for all of this!? Why do Bionis and Mechonis have to keep on fighting!? I have to... find out why!",
+          "You will know the pain and the suffering you caused the Emperor and Fiora!",
+          "What's important isn't whether you can see the future or not... It's the will to make a choice, and the strength to seize your own destiny! Our time has come. Our destiny is our own!",
+          "Today... We use our power to fell a god... and then... Seize our destiny!",
+          "I won't decide. The future should be decided by each and every person in the world. And so, what I... No, What we wish for is... A world with no gods!",
+          "That's Dunban over there!",
+          "I don't know what the future holds. But that means I can imagine the possibilities. We can achieve anything we put our minds to.",
+          "I'm really feeling it!",
+          "Monado, lend us your power!",
+          "I'll show you the power of the Monado!",
+          "Behold the power of the Monado!",
+        ],
+      },
+      {
+        character: "Klaus",
+        quotes: [
+          "Let's begin the experiment!",
+          "Ridiculous! It's perfectly safe! We are about to bear witness to the birth of a universe! Once, only a god could perform such a miracle... But today, mankind moves one step closer to the divine!",
+        ],
+      },
+      {
+        character: "Dunban",
+        quotes: [
+          "Vile Mechon! If you think the Homs... the people of Bionis... are just waiting here for you to pick us off... You are sorely mistaken!",
+          "After that battle, many of my friends did not make it home. While I was being brought here on that stretcher, they were all I thought about. [...] But I decided I would not cry. Everyone who died in that war had something they wanted to protect. Family, friends, loved ones, their home... So they fought to protect them. And we won. [...] No matter how painful, no matter how hard, it's nothing to be sad about... I decided that there and then.",
+          "Never. I will strike you down and all that you have become, you wretched fool. The Monado will stop you. And then you will know the shame of all that you have done, traitor!",
+          "It truly seems as though everything is going as Zanza planned. But our hearts, and minds... They still hold the will to seize our own destiny! No matter how hard you try to break us, the future is ours to shape, Dickson! Always!",
+          "Born In A World Of Strife! Against The Odds! We Choose To Fight! Blossom Dance!",
+          "Excuse me. We ordered room service an hour ago",
+        ],
+      },
+      {
+        character: "Egil",
+        quotes: [
+          "Your blade... It did not cut deep enough.",
+          "Shulk! Do you hear it? The awakening of the Mechonis! This is my pain. The pain of my people echoing through the millennia! Fall to your deaths! Worthless insects!",
+          "May the roar of the Mechonis be the wrath of my people!",
+          "Face me, Shulk! Successor of Zanza! I'll crush the Bionis, and spread its ashes across the world! Then I'll turn the Monado into dust. Only then will there be peace!",
+          "Your blade... It did not cut deep enough.",
+          "Face me, Shulk! Successor of Zanza! I'll crush the Bionis, and spread its ashes across the world! Then I'll turn the Monado into dust. Only then will there be peace!",
+        ],
+      },
+      {
+        character: "Meyneth",
+        quotes: [
+          "I finally understand. The futures of our children belong to them alone. Though we are their creators, their lives are theirs to control. Even if the passage of fate is as you have decided, I will teach you the strength of their will to resist. Zanza. If you choose to deny the existence of every living being but yourself... then I choose to fight. To fight for them!",
+          "This is... what I wish... This world... belongs to you all... Create a world… With no need for Gods.",
+        ],
+      },
+      {
+        character: "Alvis",
+        quotes: [
+          "Shulk. This new world is boundless. It is home to not only you, but many forms of life. I can see it. In this world, all life will walk towards the future, hand in hand.",
+          "Zanza, this is the providence of the world. Even gods are merely beings restricted to the limited power determined by providence. That power, although great, is not unlimited.",
+          "I am Monado. I was here at the beginning, and I will proclaim the end.",
+          "It was simple curiosity. The curiosity of a single man that destroyed the universe and created a new one. A new universe, your world, was born.",
+        ],
+      },
+      {
+        character: "Reyn",
+        quotes: [
+          "Now it's Reyn time!",
+          "Can't have a rainbow without Reyn baby!",
+          "Keep your stinkin' future",
+          "Man, what a bunch of jokers! ",
+        ],
+      },
+      {
+        character: "Sharla",
+        quotes: [
+          "I swear I won't stop until I've scrapped *each* and *every* one of you!",
+          "My rifle's getting hotter!",
+        ],
+      },
+      {
+        character: "Dickson",
+        quotes: [
+          "Damn. How'd that kid get so strong? I ain't gonna let you see me die. I won't give you the satisfaction of victory...See ya, kid. Looks like the student finally surpassed the teacher.",
+        ],
+      },
+      {
+        character: "Zanza",
+        quotes: [
+          "All that I am... is fading... The memory of a god's existence, born from the chaos of creation... It is...vanishing...",
+        ],
+      },
+    ],
+  },
+  {
+    game: "Xenoblade 2",
+    characters: [
+      {
+        character: "Rex",
+        quotes: [
+          "What would be the point of that? Listen, I swore to you. We're going to Elysium together! That's a promise. I'm going to Elysium for you. I'm doing all of this for you. We'll do it together! We'll find out together! We'll find your place in this world! Find out where we're headed... And see what our future holds. So believe me! I won't let the world burn a second time! So Pyra, Mythra... Join me!",
+          "I'm doing it... for myself. If it helps put smiles on people's faces... Helps them live their lives together... Then that's my role in this world!",
+          "Forgiving isn't that easy. But that's just the world we live in! We need to find a way forward, within that world! Even you... used to think like that once!",
+          "An entire horizon of white. A sea of clouds, and at its heart, the World Tree, piercing the heavens... This... This is the world we call home. Alrest.",
+        ],
+      },
+      {
+        character: "Klaus",
+        quotes: [
+          "Let's begin the experiment!",
+          "Ridiculous! It's perfectly safe! We are about to bear witness to the birth of a universe! Once, only a god could perform such a miracle... But today, mankind moves one step closer to the divine!",
+          "Galea... We humans are fools. We've ravaged the planet... We are on the verge of burning even the skies above. But the Conduit... could transform us into something so much more! Galea, this is the birth of a brand-new universe!",
+        ],
+      },
+      {
+        character: "Malos",
+        quotes: [
+          "Indeed!",
+          "This kid...HAS MADE HIMSELF THE AEGIS'S DRIVER!!!",
+          "He no longer wanted anything. He didn't even want to live anymore. And despite that, his life was the one thing he hadn't lost... because he couldn't!",
+          "Words can be a curse. That curse is what kept him tied down here.",
+          "Exactly! That's exactly it! You've gotten smarter with age, huh? I'm a wretched being too. A hideous monster, far beyond saving.",
+          "Come at me, kid! Come, my partner! Give me all you've got. You'd better show me all of it. The extent of your will. SHOW ME WHY YOU'RE HERE IN THIS WORLD",
+          `Watch humans closely and you'll learn one thing... deep down they wish they were dead. They kill each other like they swat flies. Running towards oblivion like blind rats. They see the divine flame of life and piss over it. They're geniuses at that... in a class of their own, really. Oh... that flame is wasted on them. So I... I want to give them a little push in the right direction. As the benevolent servant of our "wise dad" should...`,
+        ],
+      },
+      {
+        character: "Zeke",
+        quotes: [
+          "HANDLING A MAN'S TURTLE!",
+          "Welcome to axe town. Population: your face!",
+        ],
+      },
+      {
+        character: "Poppi",
+        quotes: [
+          "Poppi would tell you but then Poppi would have to kill you.",
+          "Poppi not born yesterday... but not much more long ago than yesterday, so not really understand",
+          "Poppi's masterpon told Poppi not to lose. So Poppi will win. Very simple logic.",
+        ],
+      },
+      {
+        character: "Nia",
+        quotes: [
+          "Here I come, ready or gormotti",
+          "I'm no longer afraid, Rex. This is who I am! What's the point in living if I have to hide?",
+          "I am burdened no longer. Not by my past, not by Jin. I am finally free.",
+        ],
+      },
+      {
+        character: "Amalthus",
+        quotes: [
+          "I often wonder if we are growing as a people... or in fact, regressing.",
+        ],
+      },
+      {
+        character: "Mòrag",
+        quotes: ["This is the culmination of years of training!"],
+      },
+      {
+        character: "Vandham",
+        quotes: [
+          "Nothin' you can do to stop this ether flow. Nothin' at all!",
+          "Remember to fight your war",
+        ],
+      },
+      {
+        character: "Jin",
+        quotes: [
+          "In the course of a lifetime, a man will see uncountable meetings and partings. Yet, as your life's candle sputters and dies, whose face is it that smiles to greet you? Happy is the man that can sleep in the comfort of the smile he sees then. No, I don't wish for 'forever.' Even just for one moment, it's enough if it's with her. And yet, what is the man to do who has not been afforded that chance? The road stretches on without end. I cannot but walk down it. As long as I can keep walking, I can hold out hope that one day, the time may come when I see her smile again.",
+        ],
+      },
+    ],
+  },
+  {
+    game: "Xenoblade 3",
+    characters: [
+      {
+        character: "Mio",
+        quotes: [
+          "Stop toying...with EVERYONE'S LIVES!!! What did they ever do to you? Why? All they wanted was their wish to be realized! You turned everything into a game! Cast them away like dolls! These are our lives here! They're not some toys you can just play with!",
+          "Your fate was sealed when you rose against us!",
+        ],
+      },
+      {
+        character: "N",
+        quotes: [
+          "Feelings, you say? You dare... My feelings... My feelings have never changed, they're the same as they've always been! They're for you, Mio! The time I had with you meant everything to me! But now...it's all gone... If she truly is gone, I'll have to redo it all! Watch me! Rewind our clocks, back to the start! Noah... My long shadow... You have stolen her away from me... I will extinguish you, and take her back! Mio is MINE! She's not yours, she's mine. You are nothing. She belongs only to ME!",
+          "Mio...! Mio, Mio, MIOOOOOO!!! YOU'RE THE ONE WHO COULDN'T PROTECT HER! In the end, you were powerless! And now she's gone...! How could you...?! ANSWER ME!",
+          "You're an off-seer, so send her.",
+        ],
+      },
+      {
+        character: "Noah",
+        quotes: [
+          "That's right. Knowing when to be close and when to let go... Walking together, eventually choosing who you really are... That is strength. And that's the real battle here... to find that answer! The will to choose our actions, moment to moment. And if you dare denounce that common right, we will defy you! To live in a future...over which you lay no claim!",
+          "This... is the truth of our world. Memories melt in the morning light, and then, a new day begins. Roads stretch out before us. So many paths. Which do you choose? That's up to you. Sometimes, you might run astray. You'll stop, maybe cry in frustration. But, you know, that's all right. For the roads... They go on without end. So look up, face forward, toward your chosen horizon... And just... walk on.",
+          "If the world holds our lives back, I will end it! It's as simple as that!",
+          "I'll tear it all down!",
+        ],
+      },
+      { character: "Sena", quotes: ["I'm the Girl with the Gall!"] },
+    ],
+  },
+];
+
+//Number to keep track of the newest created div
+let currentQuote = 0;
+
+function getQuote(game) {
+  //Gets number of quotes set by the user
+  const quoteNum = parseInt(document.querySelector("#quoteNum").value);
+  //Error message for if the user sets a number lower than 1
+  if (quoteNum < 1)
+    return (document.querySelector("#errorText").textContent =
+      "Number too low!");
+  document.querySelector("#errorText").textContent = "";
+
+  //Loops through the creation of the quotes a number of times set by the user
+  for (let i = 0; i < quoteNum; i++) {
+    //checks if the 'game' parameter is empty if it is, the gameNum variable is set to a random number.
+    const randomGNum = Math.floor(Math.random() * gameArray.length);
+    const gameNum = game === undefined ? randomGNum : game;
+    //gets a random character and quote from said character
+    const randomCNum = Math.floor(
+      Math.random() * gameArray[gameNum].characters.length
+    );
+    const randomQNum = Math.floor(
+      Math.random() * gameArray[gameNum].characters[randomCNum].quotes.length
+    );
+
+    //creates a div to contain the current quote
+    const gameContainer = document.createElement("div");
+    //gives the div an unique id to easier keep track of it
+    gameContainer.id = `quote${currentQuote}`;
+    //gives the div a generic box class and a class dependent on the game for styling
+    gameContainer.className = "box";
+    switch (gameArray[gameNum].game) {
+      case "Xenoblade 1":
+        gameContainer.className += " XC1";
+        break;
+      case "Xenoblade 2":
+        gameContainer.className += " XC2";
+        break;
+      case "Xenoblade 3":
+        gameContainer.className += " XC3";
+        break;
+    }
+    //makes the div a child of the qouteContainer div
+    document.querySelector("#quoteContainer").append(gameContainer);
+
+    //creates an h3 and puts the name of the selected game in the h3
+    const gameName = document.createElement("h3");
+    gameName.textContent = gameArray[gameNum].game;
+    //appends the h3 to the current quote
+    document.querySelector(`#quote${currentQuote}`).append(gameName);
+
+    //creates an h4 and puts the name of the selected character in the h4
+    const characterName = document.createElement("h4");
+    characterName.textContent =
+      gameArray[gameNum].characters[randomCNum].character;
+    //appends the h4 to the current quote
+    document.querySelector(`#quote${currentQuote}`).append(characterName);
+
+    //creates a pargraph and puts the selected quote in the paragraph
+    const quoteText = document.createElement("p");
+    quoteText.textContent =
+      gameArray[gameNum].characters[randomCNum].quotes[randomQNum];
+    //appends the paragraph to the current quote
+    document.querySelector(`#quote${currentQuote}`).append(quoteText);
+    //increases the current quote count by 1
+    currentQuote++;
+  }
+}
+
+//removes the children of the qouteContainer and resets the current quote count
+function clearQuotes() {
+  quoteContainer = document.querySelector("#quoteContainer").children;
+  for (let i = quoteContainer.length - 1; i >= 0; i--) {
+    quoteContainer[i].remove();
+  }
+  currentQuote = 0;
+}
